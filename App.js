1c7397463687fe52905723afee65eef1a451653a
croconut <marsteve@ucsc.edu>
2020-07-22 14:01:43 -0700

    break things horribly with flex :)

--- App.js
+++ App.js
@@ -13,16 +13,21 @@ export default function App() {
   return (
     //Note the width gets ignored cuz the flexbox only gets to control the 
     //height as they're rows
-    
+
     //added a justify content view on the text to center it
     <View style={{paddingVertical: 40, paddingHorizontal: 10,
         flexDirection: 'row', 
         justifyContent: 'space-between',
         alignItems: 'stretch', height: 300}}>
-        <View style={{justifyContent: 'center'}}><Text>112532</Text></View>
-        <TextInput placeholder="Course goal" 
-        style={{ width: "50%", borderColor: 'black', borderWidth: 2, paddingHorizontal: 10}}/>
-        <Button title="ADD" />
+        <View style={{justifyContent: 'center', flex: 5}}><Text>112532</Text></View>
+        <View style={{flex: 80, justifyContent: 'center', alignContent: 'stretch'}}>
+          <TextInput placeholder="Course goal" 
+            style={{ width: "50%", borderColor: 'black', 
+            borderWidth: 2, paddingHorizontal: 10}}/>
+        </View>
+        <View style={{flex: 20, justifyContent: 'center', alignContent: 'stretch'}}>
+          <Button title="ADD" />
+        </View>
       </View>
   );
 }
