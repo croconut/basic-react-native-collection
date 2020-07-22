54ba55f5cb8412725a6c4fba64a5bab4be2d303d
croconut <marsteve@ucsc.edu>
2020-07-22 13:53:05 -0700

    init crappy styling ex

--- App.js
+++ App.js
@@ -3,21 +3,24 @@ import { Text, TextInput, View, Button } from "react-native";
 
 /*
 Must use property names, no CSS stuff here ofc
-
+View by default uses flexbox. Organizes child elements in 1d space.
+Default is column.
 */
 
 export default function App() {
   return (
-    <View style={{padding: 30}}>
-      <View style={{flexDirection: 'row', 
+    //Note the width gets ignored cuz the flexbox only gets to control the 
+    //height as they're rows
+    <View style={{paddingHorizontal: 30,
+        flexDirection: 'row', 
         justifyContent: 'space-between',
-        alignItems: 'center'}}>
+        alignItems: 'center',
+        width: '80%', height: 300}}>
+        <Text>112532</Text>
         <TextInput placeholder="Course goal" 
         style={{ width: "70%", borderColor: 'black', borderWidth: 2, paddingHorizontal: 10}}/>
         <Button title="ADD" />
       </View>
-      <View></View>
-    </View>
   );
 }
 
