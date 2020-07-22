fcd2e50eb3a6ad5062c6a61552482cc9b68f9c79
croconut <marsteve@ucsc.edu>
2020-07-22 14:17:18 -0700

    setting up stylesheet instead

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Text, TextInput, View, Button } from "react-native";
+import { Text, StyleSheet, TextInput, View, Button } from "react-native";
 
 /*
 Must use property names, no CSS stuff here ofc
@@ -15,16 +15,26 @@ export default function App() {
     //height as they're rows
 
     //added a justify content view on the text to center it
-    <View style={{paddingVertical: 40, paddingHorizontal: 10,
-        flexDirection: 'row', 
-        justifyContent: 'space-between',
-        alignItems: 'stretch', height: 300}}>
-        <View style={{justifyContent: 'center'}}><Text>112532</Text></View>
+    <View style={styles.app}>
+      <View style={styles.topInput}>
         <TextInput placeholder="Course goal" 
-          style={{ width: "50%", borderColor: 'black', 
+          style={{ width: "80%", borderColor: 'black', 
           borderWidth: 2, paddingHorizontal: 10}}/>
         <Button title="ADD" />
       </View>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  app: {
+    paddingVertical: 30
+  },
+  topInput: {
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  }
+});
 
