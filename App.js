60df7c66e3a2400065c8a3d8b0609cf185ed7d0a
croconut <marsteve@ucsc.edu>
2020-07-22 13:57:02 -0700

    centered stuff now, items using stretch entirely fill up the space allocated

--- App.js
+++ App.js
@@ -5,20 +5,23 @@ import { Text, TextInput, View, Button } from "react-native";
 Must use property names, no CSS stuff here ofc
 View by default uses flexbox. Organizes child elements in 1d space.
 Default is column.
+
+Wow so easy to make things look like shit
 */
 
 export default function App() {
   return (
     //Note the width gets ignored cuz the flexbox only gets to control the 
     //height as they're rows
-    <View style={{paddingHorizontal: 30,
+    
+    //added a justify content view on the text to center it
+    <View style={{paddingVertical: 40, paddingHorizontal: 10,
         flexDirection: 'row', 
         justifyContent: 'space-between',
-        alignItems: 'center',
-        width: '80%', height: 300}}>
-        <Text>112532</Text>
+        alignItems: 'stretch', height: 300}}>
+        <View style={{justifyContent: 'center'}}><Text>112532</Text></View>
         <TextInput placeholder="Course goal" 
-        style={{ width: "70%", borderColor: 'black', borderWidth: 2, paddingHorizontal: 10}}/>
+        style={{ width: "50%", borderColor: 'black', borderWidth: 2, paddingHorizontal: 10}}/>
         <Button title="ADD" />
       </View>
   );
