2bcb766312641132d9f0635ed1e37682a0078bb5
croconut <marsteve@ucsc.edu>
2020-07-23 18:13:42 -0700

    using flatlist now for better performance on large/dynamic lists

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { Text, StyleSheet, TextInput, View, Button, ScrollView } from "react-native";
+import {
+  Text,
+  StyleSheet,
+  TextInput,
+  View,
+  Button,
+  FlatList,
+} from "react-native";
 
 /*
 Must use property names, no CSS stuff here ofc
@@ -7,11 +14,14 @@ View by default uses flexbox. Organizes child elements in 1d space.
 Default is column.
 
 Wow so easy to make things look like shit
+
+Scrollview is bad if theres too many elements as all the scrollview
+list items will be rendered, regardless of whether its visible to viewer
 */
 
 export default function App() {
   // examples for each of a string, array and dictionary
-  const [enteredGoal, setEnteredGoal] = useState('');
+  const [enteredGoal, setEnteredGoal] = useState("");
   const [courseGoals, setCourseGoals] = useState([]);
   const [courseGoalKeys, setCourseGoalKeys] = useState({});
 
@@ -23,72 +33,79 @@ export default function App() {
   const goalInputHandler = (text) => {
     //super simple set, we don't care about previous strings
     setEnteredGoal(text);
-  }
+  };
 
   const addGoalHandler = () => {
-
     //using dictionary to guarantee goal uniqueness now
     if (enteredGoal in courseGoalKeys) {
       return;
     }
     //maintain the previous states of the array/dicts with ...arr, newitem
-    setCourseGoals(() => [...courseGoals, enteredGoal]);
+    setCourseGoals(() => [...courseGoals, { key: enteredGoal }]);
     //this will break on tiny syntax changes, kinda finicky
-    setCourseGoalKeys(() => ( { ...courseGoalKeys, [enteredGoal]: ''} ));
-  }
+    setCourseGoalKeys(() => ({ ...courseGoalKeys, [enteredGoal]: "" }));
+  };
+
+  const goalItem = ({ item }) => (
+    <View style={styles.goalsListItem}>
+      <Text>{item.key}</Text>
+    </View>
+  );
 
   return (
-    //Note the width gets ignored cuz the flexbox only gets to control the 
+    //Note the width gets ignored cuz the flexbox only gets to control the
     //height as they're rows
 
     //added a justify content view on the text to center it
-    //can move the scrollview to the top level view to stop the top row from 
+    //can move the scrollview to the top level view to stop the top row from
     //being frozen
     <View style={styles.app}>
       <View style={styles.inputRow}>
-        <TextInput placeholder="Course goal" 
+        <TextInput
+          placeholder="Course goal"
           style={styles.textInput}
-          onChangeText={goalInputHandler} 
+          onChangeText={goalInputHandler}
           value={enteredGoal}
-          />
-        <Button title="ADD" 
-          onPress={addGoalHandler}
         />
+        <Button title="ADD" onPress={addGoalHandler} />
       </View>
-      <ScrollView style={styles.goalsList}>
-        { courseGoals.map((goal) => <View  key={goal} style={styles.goalsListItem}><Text>{goal}</Text></View>)}
-      </ScrollView>
+      <FlatList
+        data={courseGoals}
+        renderItem={goalItem}
+        keyExtractor={(item) => item.key}
+        extraData={courseGoals}
+        style={styles.goalsList}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   app: {
-    paddingVertical: 30
+    paddingVertical: 30,
   },
   inputRow: {
-    flexDirection: 'row',
+    flexDirection: "row",
     paddingHorizontal: 10,
-    justifyContent: 'space-around',
-    alignItems: 'center'
+    justifyContent: "space-around",
+    alignItems: "center",
   },
   textInput: {
-    width: "80%", 
-    borderColor: 'black', 
-    borderWidth: 2, 
-    paddingHorizontal: 10
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 2,
+    paddingHorizontal: 10,
   },
   goalsList: {
     paddingTop: 10,
-    paddingHorizontal: 5
+    paddingHorizontal: 5,
   },
   goalsListItem: {
     paddingVertical: 5,
     paddingHorizontal: 5,
     marginVertical: 3,
-    backgroundColor: '#ccc',
-    borderColor: 'black',
-    borderWidth: 1
-  }
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
+  },
 });
-
