75147c87a4c18cd7a405fbec7d5a54b1aa3e95e2
croconut <marsteve@ucsc.edu>
2020-08-21 18:34:47 -0700

    text inputs fade in/out

---
+++ src/components/FadeAnimation.js
@@ -0,0 +1,32 @@
+import React, { useRef, useEffect, useState } from "react";
+import { Animated } from "react-native";
+
+const FadeAnimation = (props) => {
+  const { visible, fadeTime } = props;
+  const fadeAnim = useRef(new Animated.Value(1)).current; // Initial value for opacity: 0
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: visible ? 1 : 0,
+      duration: fadeTime,
+      useNativeDriver: false,
+    }).start(() => {
+      setIsVisible(visible);
+    });
+  }, [fadeAnim, visible, fadeTime]);
+
+  return (
+    <Animated.View // Special animatable View
+      style={{
+        ...props.style,
+        opacity: fadeAnim, // Bind opacity to animated value
+        transform: [{ scale: fadeAnim }],
+      }}
+    >
+      {isVisible ? props.children : null}
+    </Animated.View>
+  );
+};
+
+export default FadeAnimation;
--- src/screens/AddNotes.js
+++ src/screens/AddNotes.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
-import { View, Keyboard } from "react-native";
+import { View, Keyboard, TouchableWithoutFeedback } from "react-native";
 import { Text, FAB, TextInput } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Styles from "../globals/Styles";
+import FadeAnimation from "../components/FadeAnimation";
 
 const AddNotes = ({ route, navigation }) => {
   const [noteTitle, setNoteTitle] = useState("");
   const [noteDescription, setNoteDescription] = useState("");
+  const [hideTitleInput, setHideTitleInput] = useState(false);
+  const [hideDescriptionInput, setHideDescriptionInput] = useState(false);
 
   const onSaveNote = () => {
     navigation.params.addNote({ noteTitle, noteDescription });
@@ -14,45 +17,68 @@ const AddNotes = ({ route, navigation }) => {
   };
 
   return (
-    <SafeAreaView style={Styles.container}>
-      <View style={Styles.titleContainer}>
-        <View style={{ width: "90%" }}>
-          <TextInput
-            label="Note Title"
-            value={noteTitle}
-            onChangeText={setNoteTitle}
-            mode="flat"
-          />
-          <View style={{ marginTop: "5%" }}>
-            <TextInput
-              label="Note Description"
-              value={noteDescription}
-              onChangeText={setNoteDescription}
-              multiline={true}
-              scrollEnabled={true}
-              returnKeyLabel="done"
-              blurOnSubmit={true}
-              mode="flat"
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <SafeAreaView style={Styles.container}>
+        <View style={Styles.titleContainer}>
+          <View style={{ width: "90%" }}>
+            <FadeAnimation visible={!hideTitleInput} fadeTime={200}>
+              <TextInput
+                label="Note Title"
+                value={noteTitle}
+                onFocus={() => {
+                  setHideTitleInput(false);
+                  setHideDescriptionInput(true);
+                }}
+                onBlur={() => {
+                  setHideTitleInput(false);
+                  setHideDescriptionInput(false);
+                }}
+                onChangeText={setNoteTitle}
+                mode="flat"
+              />
+            </FadeAnimation>
+            <FadeAnimation visible={!hideDescriptionInput} fadeTime={200}>
+              <View style={{ marginTop: "5%" }}>
+                <TextInput
+                  label="Note Description"
+                  value={noteDescription}
+                  onFocus={() => {
+                    setHideTitleInput(true);
+                    setHideDescriptionInput(false);
+                  }}
+                  onBlur={() => {
+                    setHideTitleInput(false);
+                    setHideDescriptionInput(false);
+                  }}
+                  onChangeText={setNoteDescription}
+                  multiline={true}
+                  scrollEnabled={true}
+                  returnKeyLabel="done"
+                  blurOnSubmit={true}
+                  mode="flat"
+                />
+              </View>
+            </FadeAnimation>
+          </View>
+        </View>
+
+        <View style={{ flex: 1 }}>
+          <View style={Styles.fabRow}>
+            <FAB icon="plus" label="add" small />
+            <FAB
+              icon="close"
+              label="cancel"
+              small
+              color={Styles.basicText.color}
+              style={Styles.fabCancel}
+              onPress={() => {
+                navigation.replace("My Notes");
+              }}
             />
           </View>
         </View>
-      </View>
-      <View style={{ flex: 1 }}>
-        <View style={Styles.fabRow}>
-          <FAB icon="plus" label="add" small />
-          <FAB
-            icon="close"
-            label="cancel"
-            small
-            color={Styles.basicText.color}
-            style={Styles.fabCancel}
-            onPress={() => {
-              navigation.replace("My Notes");
-            }}
-          />
-        </View>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 };
 
